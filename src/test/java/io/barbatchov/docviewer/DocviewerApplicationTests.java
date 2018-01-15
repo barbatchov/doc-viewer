eeedecb4fb979788c354cdae762764b6e3da9109
Elias A. Chacon <elias.alves.chacon@gmail.com>
2018-01-15 17:02:01 -0200

    Initial setup

---
+++ src/test/java/io/barbatchov/docviewer/DocviewerApplicationTests.java
@@ -0,0 +1,16 @@
+package io.barbatchov.docviewer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DocviewerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
